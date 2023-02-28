91d8331e33dc57db9b9bada3b60f54a54476649d
ChinhTheHugger <zimmerinker@gmail.com>
2023-02-28 08:49:08 +0700

    Changes from test running demo on office computer

--- demo/src/main/java/com/example/demo/DemoApplication.java
+++ demo/src/main/java/com/example/demo/DemoApplication.java
@@ -1,14 +1,14 @@
 package com.example.demo;
 
-import java.rmi.ServerException;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackages = "com.example.demo")
 public class DemoApplication {
 
-	public static void main(String[] args) throws ServerException{
+	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
 
